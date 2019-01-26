cb88af3e96be3bbbd6b00502c18ebb249442a26f
Andrew Fretwell <andrewfretwell@gmail.com>
2019-01-26 22:56:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function sayHiToGrandma(string) {
     return ("I can\'t hear you!")
      else if (string===string.toUpperCase())
      return ("YES INDEED!")
-     else (string==="I love you, Grandma.") return ("I love you, too.")
+     else if(string==="I love you, Grandma.") return ("I love you, too.")
    }
